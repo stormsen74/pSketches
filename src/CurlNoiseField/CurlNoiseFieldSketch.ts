e1579591090fc7b17ffa54a20c8003813059245d
stormsen <mst@demodern.de>
2020-01-10 20:55:37 +0100

    improve noise rendering

--- src/CurlNoiseField/CurlNoiseFieldSketch.ts
+++ src/CurlNoiseField/CurlNoiseFieldSketch.ts
@@ -16,12 +16,14 @@ export class CurlNoiseFieldSketch extends SketchTemplate {
     millis: number
     graphics: p5.Graphics
     hasBuffer: boolean
+    sampleRate: number
     t: number
 
     settings: {
         trace: boolean
         plotField: boolean
         drawNoise: boolean
+        sw: boolean
         nBlobs: number
         useCurl: boolean
         noiseScale: number
@@ -39,6 +41,7 @@ export class CurlNoiseFieldSketch extends SketchTemplate {
             trace: true,
             plotField: false,
             drawNoise: true,
+            sw: true,
             nBlobs: 100,
             useCurl: false,
             noiseScale: 0.0015,
@@ -75,16 +78,20 @@ export class CurlNoiseFieldSketch extends SketchTemplate {
     initGUI(): void {
         this.gui = new dat.GUI({ width: 350, closed: true });
         this.gui.add(this.settings, 'nBlobs', 1, 500, 1).listen()
-        this.gui.add(this.settings, 'strength', 0, 5, .1)
-        this.gui.add(this.settings, 'noiseScale', 0, .005, .0001)
+        this.gui.add(this.settings, 'strength', 0, 5, .1).onChange(() => { this.sampleRate = .1 }).onFinishChange(() => { this.sampleRate = 2 })
+        this.gui.add(this.settings, 'noiseScale', 0, .005, .0001).onChange(() => { this.sampleRate = .1 }).onFinishChange(() => { this.sampleRate = .1 })
         this.gui.add(this.settings, 'animateNoise').listen()
         this.gui.add(this.settings, 'useCurl').listen()
-        this.gui.add(this.settings, 'noiseZ', 0, 1, .01).listen().onChange(() => {
-            if (this.drawNoise) this.drawNoise()
-        })
+        this.gui.add(this.settings, 'noiseZ', 0, 1, .01).listen().onChange(() => { this.sampleRate = .1 }).onFinishChange(() => { this.sampleRate = .1 })
         this.gui.add(this.settings, 'useSimplex').listen()
         this.gui.add(this.settings, 'plotField').listen()
         this.gui.add(this.settings, 'drawNoise').listen()
+        this.gui.add(this.settings, 'sw').listen().onChange(() => {
+            this.sampleRate = .01
+            setTimeout(() => {
+                this.sampleRate = 2
+            }, 150)
+        })
         this.gui.add(this.settings, 'trace').listen()
     }
 
@@ -184,14 +191,23 @@ export class CurlNoiseFieldSketch extends SketchTemplate {
         // if (this.settings.drawNoise) this.drawNoise()
 
         const rt = this.p.millis() / 1000
-        const f = 2
+        const f = this.sampleRate
 
         if (this.settings.drawNoise) {
+
+            const sampleSize = 20
+            const dotSize = ~~this.screenSize.x / 300
+
+            const w = (this.screenSize.x / sampleSize * dotSize) + dotSize
+            const h = (this.screenSize.y / sampleSize * dotSize) + dotSize
+            const offsetX = this.screenSize.x - w - 10
+            const offsetY = this.screenSize.y - h - 10
+
             if (rt % f > f - .01) {
-                this.drawNoise()
+                this.drawNoise(sampleSize, dotSize, this.p.createVector(w, h))
             }
 
-            if (this.hasBuffer) this.p.image(this.graphics, 5, 30);
+            if (this.hasBuffer) this.p.image(this.graphics, offsetX, offsetY);
 
         }
 
@@ -221,19 +237,7 @@ export class CurlNoiseFieldSketch extends SketchTemplate {
         }
     }
 
-    private drawNoise(): void {
-
-
-        const sampleSize = 20
-        // const dotSize = 5
-        const dotSize = ~~this.screenSize.x / 300
-        console.log(dotSize)
-
-        const w = (this.screenSize.x / sampleSize * dotSize) + dotSize
-        const h = (this.screenSize.y / sampleSize * dotSize) + dotSize
-        const offsetX = this.screenSize.x - w
-        const offsetY = this.screenSize.y - h
-
+    private drawNoise(sampleSize: number, dotSize: number, size: p5.Vector): void {
 
 
         const updateRender = () => {
@@ -243,23 +247,31 @@ export class CurlNoiseFieldSketch extends SketchTemplate {
                     const position = this.p.createVector(k, j)
                     const pScaled = position.copy().mult(this.settings.noiseScale);
                     const noise = !this.settings.useSimplex ? this.getNoise(pScaled.x, pScaled.y) : this.p.map(this.getNoise(pScaled.x, pScaled.y), -1, 1, 0, 1)
-                    
-                    const vResult = this.curlField ? this.curlField(pScaled) : this.getVel(pScaled)
-                    const mag = vResult.mag() * .5
-                    // let angle = vResult.heading();
-                    const lum = this.p.map(mag, 0, 1, 0, .5)
-                    const mappedHue = this.p.map(noise, 0, 1, 0, .5)
-                    this.p.colorMode(this.p.HSL, 1)
-                    const colorVelocity = this.p.color(mappedHue, .5, lum, 1)
+
+
+
+                    let usedColor: p5.Color
+                    if (this.settings.sw) {
+                        // noise
+                        usedColor = this.p.color(noise * 255, noise * 255, noise * 255, 200)
+                    } else {
+                        const vResult = this.curlField ? this.curlField(pScaled) : this.getVel(pScaled)
+                        const mag = vResult.mag() * .5
+                        // let angle = vResult.heading();
+                        const lum = this.p.map(mag, 0, 1, 0, .5)
+                        const mappedHue = this.p.map(noise, 0, 1, 0, .5)
+
+                        this.p.colorMode(this.p.HSL, 1)
+                        usedColor = this.p.color(mappedHue, .5, lum, 1)
+                    }
+
+                    // const colorVelocity = this.p.color(mappedHue, .5, lum, 1)
                     // console.log(colorVelocity)
-                    
-                    
-                    
-                    const colorNoise = this.p.color(noise * 255, noise * 255, noise * 255, 200)
-                    
+
+
                     //draw
                     this.graphics.noStroke()
-                    this.graphics.fill(colorVelocity)
+                    this.graphics.fill(usedColor)
                     this.graphics.rect(k / sampleSize * dotSize, j / sampleSize * dotSize, dotSize, dotSize)
 
                     this.p.colorMode(this.p.RGB, 255)
@@ -267,62 +279,18 @@ export class CurlNoiseFieldSketch extends SketchTemplate {
                 }
             }
 
-            this.graphics.stroke(this.p.color(255, 0, 0, 255))
-            this.graphics.strokeWeight(1)
-            this.graphics.noFill()
-            this.graphics.rect(0, 0, w, h - 1)
-
-
+            // this.graphics.stroke(this.p.color(255, 0, 0, 255))
+            // this.graphics.strokeWeight(1)
+            // this.graphics.noFill()
+            // this.graphics.rect(0, 0, size.x, size.y)
 
         }
 
-        this.graphics = this.p.createGraphics(w + 3, h + 3);
-
-        // this.graphics.background(51);
-        // this.graphics.noFill();
-        // this.graphics.stroke(255);
-        // this.graphics.ellipse(this.p.mouseX - 150, this.p.mouseY - 75, 60, 60);
+        this.graphics = this.p.createGraphics(size.x + 3, size.y + 3);
 
         this.hasBuffer = true;
         updateRender()
 
-
-
-        // this.p.push()
-        // this.p.translate(offsetX - 1, offsetY - 1)
-        // this.p.colorMode(this.p.HSL, 1)
-
-        // for (let k = 0; k <= this.screenSize.x; k += sampleSize) {
-        //     for (let j = 0; j <= this.screenSize.y; j += sampleSize) {
-        //         const position = this.p.createVector(k, j)
-        //         const pScaled = position.copy().mult(this.settings.noiseScale);
-        //         const noise = !this.settings.useSimplex ? this.getNoise(pScaled.x, pScaled.y) : this.p.map(this.getNoise(pScaled.x, pScaled.y), -1, 1, 0, 1)
-
-        //         const vResult = this.curlField ? this.curlField(pScaled) : this.getVel(pScaled)
-        //         const mag = vResult.mag() * .5
-        //         // let angle = vResult.heading();
-        //         const lum = this.p.map(mag, 0, 1, 0, .5)
-        //         const mappedHue = this.p.map(noise, 0, 1, 0, .5)
-        //         const colorVelocity = this.p.color(mappedHue, .5, lum, 1)
-
-
-
-        //         const colorNoise = this.p.color(noise * 255, noise * 255, noise * 255, 200)
-
-        //         //draw
-        //         this.p.noStroke()
-        //         this.p.fill(colorNoise)
-        //         this.p.rect(k / sampleSize * dotSize, j / sampleSize * dotSize, dotSize, dotSize)
-
-        //     }
-        // }
-
-        // this.p.stroke(this.p.color(255, 0, 0, 255))
-        // this.p.strokeWeight(1)
-        // this.p.noFill()
-        // this.p.rect(0, 0, w, h)
-
-        // this.p.pop();
     }
 
     destroy() {
